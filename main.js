67110face05e3acdc39e50d468a702fe58df3c7f
Heestolee <dlckdgml25@gmail.com>
2025-05-09 19:48:01 +0900

    Refactor: addEntry 기능 features/addEntry.js로 분리

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
-import { getData, setData } from "./store/dataStore.js";
+import { setData } from "./store/dataStore.js";
 import { syncAll } from "./render/syncAll.js";
 import { applyTableChanges } from "./features/applyTableChanges.js";
-
-function addEntry() {
-  const idInputEl = document.getElementById("id-input");
-  const valueInputEl = document.getElementById("value-input");
-  const id = parseInt(idInputEl.value);
-  const value = parseInt(valueInputEl.value);
-
-  if (isNaN(id) || isNaN(value)) {
-    alert("숫자를 입력하세요.");
-    return;
-  }
-
-  const currentData = getData();
-  if (currentData.some((entry) => entry.id === id)) {
-    alert("중복된 ID입니다.");
-    return;
-  }
-
-  setData([...currentData, { id, value }]);
-  idInputEl.value = "";
-  valueInputEl.value = "";
-  syncAll();
-}
+import { addEntry } from "./features/addEntry.js";
 
 function applyJsonEdit() {
   try {
